63e588330962d793aad8c2bad247b2d19fe0bf2e
fiona <fiona.mburu@student.moringaschool.com>
2024-07-25 16:37:13 +0300

    acts on events

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft(){
+    const leftNum = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNum, 10);
+
+    if(left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNum = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNum, 10);
+
+    if(left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
